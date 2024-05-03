bba78060b5d9ce8ebc9c7f66d6edf12adf7ad9ca
Bren Broussard <broussard.j.bren@gmail.com>
2024-05-03 16:26:42 -0500

    generate svg code added

--- index.js
+++ index.js
@@ -35,4 +35,31 @@ async function promptUser() {
     return answers;
 }
 
-async
+async function generateSVG() {
+    const userInput = await promptUser();
+    let shape;
+    switch (userInput.shape) {
+        case 'Triangle':
+            shape = new Triangle();
+            break;
+        case 'Circle':
+            shape = new Circle();
+            break;
+        case 'Square':
+            shape = new Square();
+            break;
+    }
+    shape.setColor(userInput.shapeColor);
+
+    const svgContent = `
+    <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+        ${shape.render()}
+        <text x="150" y="150" font-family="Arial" font-size="24" fill="${userInput.textColor}" text-anchor="middle">${userInput.text}</text>
+    </svg>
+`;
+fs.writeFileSync('logo.svg',svgContent);
+
+console.log("Generated logo.svg");
+}
+
+generateSVG();
